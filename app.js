acac49ca45541ba3dae4c10c987bde5581c23185
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-08-29 02:03:33 -0700

    Practice

--- app.js
+++ app.js
@@ -32,3 +32,18 @@ promise.then(res => {
 }).catch(rej =>{
   console.log('error', rej);
 })
+
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1
+  }),
+  headers: {
+    "Content-type": "application/json"
+  }
+})
+.then(response=> response.json())
+.then(data => console.log(data))
